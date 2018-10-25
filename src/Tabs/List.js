9bda0a855e0bc3fdc5beec19495e957ba7ad5ff5
Cícero Akiles <cicero.akiles@gmail.com>
2018-10-25 01:22:31 -0300

    Inclusão na listagem

--- src/Tabs/List.js
+++ src/Tabs/List.js
@@ -1,21 +1,50 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import { toList } from '../Tabs/Scan';
+import { ExportedScanList } from '../Tabs/Scan';
+
+// TODO: Identificação via bd.
+function nomear(code) {
+    switch (code) {
+        case "9788538064718": return "A ARTE DA GUERRA";
+        case "7898916448732": return "SUDOKU (MÉDIO/DIFÍCIL)";
+    }
+}
+
+function Item(props) {
+    return (
+      <Text className="item">
+        { nomear(props.texto) }
+      </Text>
+    );
+  }
 
 export default class List extends React.Component {
-    render() {
-        var test;
-        if (toList == undefined) {
-            test = "";
+    uniqueItems(duplicates) {
+        if (duplicates) {
+          var unique = duplicates.filter(function(elem, pos) {
+            return duplicates.indexOf(elem) == pos;
+          });
+          return unique;
         } else {
-            test = toList[0];
+          return [];
         }
-        return(
+    }
+
+    // TODO: Atualização dos itens.
+    render() {
+        var content = [];
+        if (ExportedScanList) {
+            var temp = this.uniqueItems(ExportedScanList); // this.state.data);
+            for (var i = 0; i < temp.length; i++) {
+                content.push(<Item key={i} texto={ temp[i] } />);
+            }
+        }
+        return (
             <View style={styles.container}>
                 <Text style={styles.title}>Itens</Text>
-                <Text style={styles.item}>{ test }</Text>
+                { content }
             </View>
-        );
+        ); // <div key={id}>{ content }</div> // id=length?
     }
 }
 
@@ -30,7 +59,8 @@ const styles = StyleSheet.create({
         margin: 10
     },
     item: {
-        fontSize: 20,
+        fontSize: 12,
         textAlign: 'left',
+        margin: 5
     }
 });
--- src/Tabs/Scan.js
+++ src/Tabs/Scan.js
@@ -2,12 +2,11 @@ import React from 'react';
 import { StyleSheet, View, Text, Alert } from 'react-native';
 import { BarCodeScanner, Permissions } from 'expo';
 import { List } from '../utils/firebaseService';
-//import { FDatabase } from '../utils/config';
+//import { FDatabase } from '../utils/config'; // add_data
 
 export default class Scan extends React.Component {
   state = {
-      hasCameraPermission: null,
-      data: []
+    hasCameraPermission: null //, data: []
   };
 
   componentDidMount() {
@@ -22,34 +21,21 @@ export default class Scan extends React.Component {
   };
 
   _handleBarCodeRead = jsonData => {
-    //var itemsRef = FDatabase.ref('barcodes');
-    //itemsRef.push(jsonData);
-    var flag = false;
-    if (List.includes(jsonData["data"])) {
-      this.state.data.push(jsonData["data"]); // FORCE
-      flag = true;
+    //var itemsRef = FDatabase.ref('barcodes'); // add_data
+    //itemsRef.push(jsonData); // add_data
+    if (!List.includes(jsonData["data"])) {
+      console.log("AVISO: Sucesso na leitura, porém indisponível no bd.");
     } else {
-      console.log("AVISO: Sucesso na leitura, porém indisponível no banco.");
+      //this.state.data.push(jsonData["data"]); // FORCE
+      ExportedScanList.push(jsonData["data"]);
+      Alert.alert('Leitura concluída!'); //, JSON.stringify(jsonData));
+      console.log(ExportedScanList); // this.state.data);
     }
-    console.log(this.state.data);
-    if (flag) Alert.alert('Leitura concluída!'); //, JSON.stringify(jsonData));
   };
-
-  // TODO: Mover pra outro js.
-  uniqueItems(duplicates) {
-    if (duplicates) {
-      var unique = duplicates.filter(function(elem, pos) {
-        return duplicates.indexOf(elem) == pos;
-      });
-      return unique;
-    } else {
-      return [];
-    }
-  }
 
   render() {
     return (
-        
+
       <View style={styles.container}>
         {this.state.hasCameraPermission === null ?
           <Text>Aceitar permissão de câmera</Text> :
@@ -61,16 +47,18 @@ export default class Scan extends React.Component {
             />
         }
       </View>
-      
+
     );
   }
 
-  /* handleBarCodeScanned = ({ type, data }) => {
+  /*
+  handleBarCodeScanned = ({ type, data }) => {
     alert(`Bar code with type ${type} and data ${data} has been scanned!`);
-  } */
+  }
+  */
 }
 
-export const toList = ["(nada)", "(mais nada)", "(mais nada ainda)"];
+export var ExportedScanList = [];
 
 const styles = StyleSheet.create({
   container: {
